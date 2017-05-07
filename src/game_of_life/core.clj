d5e02928284d333255150185ba55f06a0fe38221
Akshay Chiwhane <achiwhane@gmail.com>
2017-05-06 21:19:51 -0400

    Add two versions of core loop that use reduce instead of indexing

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -45,6 +45,41 @@
             (recur (assoc-in new-board [x y] new-liveness) x (inc y)))))))
 
 
+;; replace loops with a nested reduce call
+(defn indexed-step-with-reduce
+  "Yields the next state of the board, using indices to determine neighbors"
+  [board]
+  (let [w (count board)
+        h (count (first board))]
+    (reduce
+      (fn [new-board x]
+        (reduce
+          (fn [new-board y]
+            (let [new-liveness
+                  (case (count-neighbors board [x y])
+                    2 (get-in board [x y])
+                    3 :on
+                    nil)]
+              (assoc-in new-board [x y] new-liveness)))
+          new-board (range h)))
+      board (range w))))
+
+(defn indexed-step-with-reduce-collapsed
+  [board]
+  (let [w (count board)
+        h (count (first board))]
+    (reduce
+      (fn [new-board [x y]]
+        (let [new-liveness
+              (case (count-neighbors board [x y])
+                2 (get-in board [x y])
+                3 :on
+                nil)]
+          (assoc-in new-board [x y] new-liveness)))
+      board (for [x (range w) y (range h)] [x y]))))
+
+
+
 (def glider (populate (empty-board 6 6) #{[2 0] [2 1] [2 2] [1 2] [0 1]}))
 (-> (iterate indexed-step glider) (nth 8) pprint)
 
